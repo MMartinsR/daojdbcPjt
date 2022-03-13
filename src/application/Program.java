5fd547c0bd01d1e0cc4064812c421c6741597575
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-03-13 20:06:06 +0000

    Creating project and git repository

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		/* DAO
+		 * Para cada entidade, haverá um objeto responsável por fazer acesso a dados relacionado a esta entidade. Por exemplo:
+		 * Cliente: ClienteDAO
+		 * Produto: ProdutoDAO
+		 * Pedido: PedidoDAO
+		 * 
+		 * Cada DAO será definido por uma interface.
+		 * A injeção de dependência pode ser feita por meio do padrao de projeto Factory.
+		 */
+	}
+
+}
