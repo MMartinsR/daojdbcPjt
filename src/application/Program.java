df3d0b38a00339419fe2661a28fb6b5c71b9827a
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-03-13 23:52:30 +0000

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,37 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		/* DAO
-		 * Para cada entidade, haverá um objeto responsável por fazer acesso a dados relacionado a esta entidade. Por exemplo:
-		 * Cliente: ClienteDAO
-		 * Produto: ProdutoDAO
-		 * Pedido: PedidoDAO
+
+		/*
+		 * DAO Para cada entidade, haverá um objeto responsável por fazer acesso a dados
+		 * relacionado a esta entidade. Por exemplo: Cliente: ClienteDAO Produto:
+		 * ProdutoDAO Pedido: PedidoDAO
 		 * 
-		 * Cada DAO será definido por uma interface.
-		 * A injeção de dependência pode ser feita por meio do padrao de projeto Factory.
+		 * Cada DAO será definido por uma interface. A injeção de dependência pode ser
+		 * feita por meio do padrao de projeto Factory.
 		 */
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findById ====");
-		
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+
+		System.out.println("\n=== TEST 2: seller findByDepartment ====");
+		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 	}
 
 }
